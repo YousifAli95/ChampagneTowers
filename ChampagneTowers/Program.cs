4a88f86b7ac51b1056ca9e4752722bd762516f2a
Yousif Ali <Yousifali95@hotmail.co.uk>
2022-12-06 22:58:40 +0100

    Make the code more fast and exact

--- ChampagneTowers/Program.cs
+++ ChampagneTowers/Program.cs
@@ -14,64 +14,88 @@
                 champagneTower[row] = new double[row + 1];
             }
             int numberOfGlassesPoured = 0;
+            double stepSize = 1;
             //for (int numberOfGlassesPoured = 0; numberOfGlassesPoured < 20; numberOfGlassesPoured++)
             var fullGlass = false;
             while (!fullGlass)
             {
-                //Console.WriteLine($"Glass number: {numberOfGlassesPoured}");
-                Console.WriteLine($"Number of second passed: {(numberOfGlassesPoured + 1) * 10}s");
-                PourGlass(champagneTower);
+                numberOfGlassesPoured++;
+                PourGlass(champagneTower, stepSize);
                 if (champagneTower[chosenRow - 1][chosenGlass - 1] == 1)
                 {
                     fullGlass = true;
+                    numberOfGlassesPoured--;
                 }
-                else
+                //PrintChampagneTower(champagneTower);
+            }
+            double[][] newChampagneTower = new double[numberOfRows][];
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                newChampagneTower[row] = new double[row + 1];
+            }
+            int newNumberOfGlassesPoured = 0;
+            for (int i = 0; i < numberOfGlassesPoured; i++)
+            {
+                Console.WriteLine($"Number of second passed: {((newNumberOfGlassesPoured++ + 1) * 10 * stepSize).ToString("#0.000")}s");
+                PourGlass(newChampagneTower, stepSize);
+                PrintChampagneTower(newChampagneTower);
+            }
+            stepSize = 0.0001;
+            fullGlass = false;
+            numberOfGlassesPoured = 0;
+            double numberOfSecondsPassed = stepSize + newNumberOfGlassesPoured;
+            //(numberOfSecondsPassed * 10 * stepSize).ToString("#0.000")
+            while (!fullGlass)
+            {
+                Console.WriteLine($"Number of second passed:  {numberOfSecondsPassed * 10}s");
+                numberOfSecondsPassed += stepSize;
+                PourGlass(newChampagneTower, stepSize);
+                if (newChampagneTower[chosenRow - 1][chosenGlass - 1] == 1)
                 {
-                    numberOfGlassesPoured++;
+                    fullGlass = true;
+                    //numberOfGlassesPoured--;
                 }
                 PrintChampagneTower(champagneTower);
             }
-            //if()
-
-
-        }
-
-        static void PrintChampagneTower(double[][] champagneTower)
-        {
-
-            foreach (var row in champagneTower)
+            numberOfSecondsPassed -= stepSize;
+            Console.WriteLine($"Number of second passed:  {(numberOfSecondsPassed * 10).ToString("#0.000")}s");
+            static void PrintChampagneTower(double[][] champagneTower)
             {
-                foreach (var glass in row)
-                {
-                    Console.Write($"[{glass}] ");
-                }
-                Console.WriteLine();
-            }
-        }
 
-        static void PourGlass(double[][] champagneTower)
-        {
-            champagneTower[0][0] += 1;
-            for (int row = 0; row < champagneTower.Length - 1; row++)
-            {
-                for (int glass = 0; glass < champagneTower[row].Length; glass++)
+                foreach (var row in champagneTower)
                 {
-                    //double fillAmount = Math.Min(1, champagneTower[row][glass]);
-                    //double overflowAmount = Math.Max(0, champagneTower[row][glass] - 1);
-
-                    var tmp = (champagneTower[row][glass] - 1) / 2;
-                    if (tmp > 0)
+                    foreach (var glass in row)
                     {
-                        champagneTower[row + 1][glass] += tmp;
-                        champagneTower[row + 1][glass + 1] += tmp;
+                        Console.Write($"[{glass}] ");
                     }
-                    champagneTower[row][glass] = Math.Min(1, champagneTower[row][glass]);
+                    Console.WriteLine();
+                }
+            }
 
+            static void PourGlass(double[][] champagneTower, double StepSize)
+            {
+                champagneTower[0][0] += StepSize;
+                for (int row = 0; row < champagneTower.Length - 1; row++)
+                {
+                    for (int glass = 0; glass < champagneTower[row].Length; glass++)
+                    {
+                        //double fillAmount = Math.Min(1, champagneTower[row][glass]);
+                        //double overflowAmount = Math.Max(0, champagneTower[row][glass] - 1);
+
+                        var tmp = (champagneTower[row][glass] - 1) / 2;
+                        if (tmp > 0)
+                        {
+                            champagneTower[row + 1][glass] += tmp;
+                            champagneTower[row + 1][glass + 1] += tmp;
+                        }
+                        champagneTower[row][glass] = Math.Min(1, champagneTower[row][glass]);
+
+
+                    }
 
                 }
 
             }
-
         }
     }
 }
